4db21052f5a34ead0e067d3f2aa67e82bd9ae00a
Alexis De_Temmerman <de.temmerman.alexis@gmail.com>
2024-02-16 23:40:59 +0100

    Working game with history

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function Square({value, onSquareClick}) {
   );
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+function Board({xIsNext, squares, onPlay}) {
   const winner = calculateWinner(squares);
   let status;
 
@@ -41,7 +39,6 @@ export default function Board() {
     </>
   );
 
-
   function handleClick(i) {
     const nextSquares = squares.slice();
 
@@ -55,8 +52,7 @@ export default function Board() {
       nextSquares[i] = 'O';
     }
 
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   function calculateWinner(squares) {
@@ -71,8 +67,11 @@ export default function Board() {
       [2, 4, 6]
     ]
 
+    console.log(squares)
+
     for (let iLine = 0; iLine < lines.length; iLine++) {
       const [a, b, c] = lines[iLine];
+
 
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
@@ -80,4 +79,50 @@ export default function Board() {
     }
     return null;
   }
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = ([...history.slice(0, currentMove + 1), nextSquares]);
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  })
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className='game-info'>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
 }
